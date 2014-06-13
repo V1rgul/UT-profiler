7a8f0057d6aea9f3b0e1e48350db57201efa3944
Carl Levasseur <levasseur.cl@gmail.com>
2014-06-13 16:42:18 +0200

    Création d'utilisateurs

--- src/main.cpp
+++ src/main.cpp
@@ -12,75 +12,64 @@
 #include "controller/mainwindowcontroller.h"
 
 int main(int argc, char *argv[]) {
-	QApplication a(argc, argv);
+  QApplication a(argc, argv);
 
-	/*
-	Catalogue* c;
-	try {
-	c = Catalogue::charger();
-	} catch (const char* e) {
-	qDebug() << e;
-	}
 
-	QMap<QString, UV> uvs = c->uvs();
-	QList<QString> keys = uvs.keys();
+  Catalogue* c;
+  try {
+    c = Catalogue::charger();
+  } catch (const char* e) {
+    qDebug() << e;
+  }
 
-	for (int i = 0; i < keys.count(); i++) {
-	UV u = uvs[keys[i]];
-	qDebug() << u.tag() << ": " << u.titre() << "\tcredits: " << u.credits()
-			 << "\tcursus: " << u.cursus().join(" ") << "\tautomne: " << u.automne()
-			 << "\tprintemps: " << u.printemps();
-	}
+  Etudiant e;
+  e.nom("Stevie");
+  e.prenom("Wonder");
 
-	Etudiant e;
-	e.nom("Dupont");
-	e.prenom("Jean");
+  Formation f;
+  Semestre s1, s2;
+  UVEtudiant uv1, uv2, uv3, uv4, uv5, uv6;
 
-	Formation f;
-	Semestre s1, s2;
-	UVEtudiant uv1, uv2, uv3, uv4, uv5, uv6;
+  uv1.tag("PS91"); uv1.note("B");
+  uv2.tag("PH09"); uv2.note("FX");
+  uv3.tag("NF24"); uv3.note("C");
+  uv4.tag("SA11"); uv4.note("A");
+  uv5.tag("LG30");
+  uv6.tag("MC05");
 
-	uv1.tag("MB11");
-	uv2.tag("NF16");
-	uv3.tag("SR01");
-	uv4.tag("LO21");
-	uv5.tag("NF17");
-	uv6.tag("SR02");
-	//uv.note(EN_COURS);
+  s1.ajouterUv(&uv1);
+  s1.ajouterUv(&uv2);
+  s1.ajouterUv(&uv3);
+  s2.ajouterUv(&uv4);
+  s2.ajouterUv(&uv5);
+  s2.ajouterUv(&uv6);
 
-	s1.ajouterUv(&uv1);
-	s1.ajouterUv(&uv2);
-	s1.ajouterUv(&uv3);
-	s2.ajouterUv(&uv4);
-	s2.ajouterUv(&uv5);
-	s2.ajouterUv(&uv6);
+  f.ajouterSemestre(&s1);
+  f.ajouterSemestre(&s2);
 
-	f.ajouterSemestre(&s1);
-	f.ajouterSemestre(&s2);
+  e.ajouterFormation(f);
 
-	e.ajouterFormation(f);
+  try {
+    e.sauvegarder();  //test la sauvegarde d'un etudiant dans un fichier xml
+    Etudiant *e1 = Etudiant::charger("Abou Baker"); // ainsi que le chargement d'un fichier xml
+    qDebug() << "nom: " << e1->nom() << " prenom: " << e1->prenom();
+  }
+  // TODO: Ameliorer la gestion d'erreur
+  catch (QString& e) {
+    QMessageBox box;
+    box.setIcon(QMessageBox::Critical);
+    box.setText(e);
+    box.exec();
+  } catch (const char* e2) {
+    QMessageBox box;
+    box.setIcon(QMessageBox::Critical);
+    box.setText(e2);
+    box.exec();
+  }
 
-	try {
-	e.sauvegarder(); // test la sauvegarde d'un etudiant dans un fichier xml
-	//Etudiant *e1 = Etudiant::charger("../test.xml"); // ainsi que le chargement d'un fichier xml
-	//qDebug() << "nom: " << e1->nom() << " prenom: " << e1->prenom();
-	}
-	// TODO: Ameliorer la gestion d'erreur
-	catch (QString& e) {
-	QMessageBox box;
-	box.setIcon(QMessageBox::Critical);
-	box.setText(e);
-	box.exec();
-	} catch (const char* e2) {
-	QMessageBox box;
-	box.setIcon(QMessageBox::Critical);
-	box.setText(e2);
-	box.exec();
-	}
-	*/
 
-	MainWindowController mainWindowController(a);
-	Q_UNUSED(mainWindowController);
+  MainWindowController mainWindowController(a);
+  Q_UNUSED(mainWindowController);
 
-	return a.exec();
+  return a.exec();
 }
